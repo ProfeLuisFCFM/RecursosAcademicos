1ae2d2a16a50a22672b33af3b835de3e9ae87ff9
HP240G <cs113invop@gmail.com>
2025-02-21 21:15:52 -0600

    subir parcial

---
+++ ProgramacionBasica/1erParcial.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import sys
+
+# Lista de librerías que deseas instalar
+libraries = [
+    'wget',
+    'pyqrcode'
+]
+
+# Función para instalar las librerías si no están ya instaladas
+def requisitos():
+    for lib in libraries:
+        try:
+            # Intentamos importar la librería
+            __import__(lib)
+            print(f"{lib} ya está instalada ✅")
+        except ImportError:
+            # Si no está instalada, la instalamos
+            print(f"Instalando {lib}...")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
+            print(f"{lib} instalada correctamente ✅")
+
+# Llamamos a la función para instalar las librerías necesarias
+#requisitos()
